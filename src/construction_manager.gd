08f81ff723003712945fb504253d5aec9e8cefc8
juxxon23 <juxxon23@hotmail.com>
2025-08-02 13:47:45 -0500

    Added quadrant groups and collision deactivation for colors other than the current one

--- src/construction_manager.gd
+++ src/construction_manager.gd
@@ -10,28 +10,32 @@ func _ready() -> void:
 	set_initial_pieces()
 	
 	
-# Para multiplayer se crearan dos grupos "r"/"l" de acuerdo a su cuadrante y se
-# agregaran las instancias de las piezas a dichos grupos
+func random_position_in_right_quadrant() -> Vector2:
+	return Vector2(randi_range(656, 1096),randi_range(104, 624))	
+
+	
 func set_initial_pieces() -> void:
 	for i in 16:
 		var piece = piece_scene.instantiate()
-		# Agregar a grupo right o left
 		piece.change_color(piece_colors.pick_random())
 		# avoid overlapping (todo)
 		piece.position = random_position_in_right_quadrant()
+		piece.add_to_group("right")
 		add_child(piece)
-		
-
-func random_position_in_right_quadrant() -> Vector2:
-	return Vector2(randi_range(656, 1096),randi_range(104, 624))
 	
+
+func group_collision(group: String, q: int) -> void:
+	get_tree().call_group(group, "disable_collision_by_color", current_color[q])
+
 
 func current_qcolor(arg1, arg2) -> void: # arg1: quadrant, arg2: current_color
 	# Segun cuadrante enviar senal al grupo del color actual
 	if arg1 == "l":
 		current_color[0] = arg2
+		group_collision("left", 0)
 	elif arg1 == "r":
 		current_color[1] = arg2
+		group_collision("right", 1)
 	else:
 		return
 	
--- src/piece.gd
+++ src/piece.gd
@@ -4,15 +4,20 @@ extends Area2D
 func change_color(color: String) -> void:
 	$ColorRect.color = Color.html(color)
 
-
-func toogle_collision(opt: bool) -> void:
-	$CollisionShape2D.set_deferred("disabled", opt)
 	
+func format_color(color: Color) -> String:
+	return "#" + color.to_html(false)
+
 
 func _on_body_entered(body: Node2D) -> void:
-	var color_piece = "#" + $ColorRect.color.to_html(false)
+	var color_piece = format_color($ColorRect.color)
 	get_tree().call_group("builders", "pick_up_pieces", [body, color_piece])
 
 
-# La pieza verificara si su color corresponde al actual, si no lo es entonces
-# desactivara su colision.
+func disable_collision(opt: bool) -> void:
+	$CollisionShape2D.set_deferred("disabled", opt)
+
+
+func disable_collision_by_color(current_color: String) -> void:
+	if format_color($ColorRect.color) != current_color:
+		disable_collision(true)	
